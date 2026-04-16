5cb731142d17b1437fcc9ff8de8c869be27318e8
Johnny_Ma <johnny880319@gmail.com>
2026-04-16 22:02:31 +0800

    codecrafters submit [skip ci]

--- cmd/my_bittorrent/main.go
+++ cmd/my_bittorrent/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/codecrafters-io/bittorrent-starter-go/internal/parse"
 	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
@@ -23,6 +24,22 @@ func main() {
 
 		jsonOutput, _ := json.Marshal(decoded)
 		fmt.Println(string(jsonOutput))
+	} else if command == "info" {
+		file_name := os.Args[2]
+		file_bytes, err := os.ReadFile(file_name)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		decoded, _, err := parse.DecodeBencode(string(file_bytes), 0)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println("Tracker URL: " + decoded.(map[string]interface{})["announce"].(string))
+		fmt.Println("Length: " + strconv.Itoa(decoded.(map[string]interface{})["info"].(map[string]interface{})["length"].(int)))
 	} else {
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
