dcf1ad4a117e24ab8551ad664e6fd4b6a7f06b37
Johnny_Ma <johnny880319@gmail.com>
2026-04-24 01:43:31 +0800

    codecrafters submit [skip ci]

--- cmd/my_bittorrent/command.go
+++ cmd/my_bittorrent/command.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net"
 	"os"
 	"strconv"
 
@@ -15,56 +16,33 @@ import (
 func cmdDecode() {
 	bencodedValue := os.Args[2]
 	decoded, _, err := bencode.DecodeBencode(bencodedValue, 0)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	die(err)
 
 	jsonOutput, _ := json.Marshal(decoded)
 	fmt.Println(string(jsonOutput))
 }
 
 func cmdInfo() {
-	fileName := os.Args[2]
-	//nolint:gosec // This is a command-line tool. We trust the user to provide a valid file path.
-	fileBytes, err := os.ReadFile(fileName)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	filePath := os.Args[2]
+	metaInfo, err := loadTorrent(filePath)
+	die(err)
 
-	torrentInfo, err := metainfo.Parse(fileBytes)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println("Tracker URL: " + torrentInfo.TrackerURL)
-	fmt.Println("Length: " + strconv.Itoa(torrentInfo.Length))
-	fmt.Println("Info Hash: " + fmt.Sprintf("%x", torrentInfo.InfoHash))
-	fmt.Println("Piece Length: " + strconv.Itoa(torrentInfo.PieceLength))
+	fmt.Println("Tracker URL: " + metaInfo.TrackerURL)
+	fmt.Println("Length: " + strconv.Itoa(metaInfo.Length))
+	fmt.Println("Info Hash: " + fmt.Sprintf("%x", metaInfo.InfoHash))
+	fmt.Println("Piece Length: " + strconv.Itoa(metaInfo.PieceLength))
 	fmt.Println("Piece Hashes:")
-	for i := 0; i < len(torrentInfo.PieceHashes); i++ {
-		fmt.Printf("%x\n", torrentInfo.PieceHashes[i])
+	for i := 0; i < len(metaInfo.PieceHashes); i++ {
+		fmt.Printf("%x\n", metaInfo.PieceHashes[i])
 	}
 }
 
 func cmdPeers() {
-	fileName := os.Args[2]
-	//nolint:gosec // This is a command-line tool. We trust the user to provide a valid file path.
-	fileBytes, err := os.ReadFile(fileName)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	filePath := os.Args[2]
+	metaInfo, err := loadTorrent(filePath)
+	die(err)
 
-	torrentInfo, err := metainfo.Parse(fileBytes)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	peers, err := tracker.GetPeers(torrentInfo)
+	peers, err := tracker.GetPeers(metaInfo)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -76,26 +54,14 @@ func cmdPeers() {
 }
 
 func cmdHandshake() {
-	fileName := os.Args[2]
+	filePath := os.Args[2]
 	peerIP := os.Args[3]
-	//nolint:gosec // This is a command-line tool. We trust the user to provide a valid file path.
-	fileBytes, err := os.ReadFile(fileName)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	metaInfo, err := loadTorrent(filePath)
+	die(err)
 
-	torrentInfo, err := metainfo.Parse(fileBytes)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	conn, peerID, err := peer.Dial(peerIP, metaInfo)
+	die(err)
 
-	conn, peerID, err := peer.Dial(peerIP, torrentInfo)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
 	defer func() {
 		_ = conn.Close()
 	}()
@@ -108,124 +74,97 @@ func cmdDownloadPiece() {
 		os.Exit(1)
 	}
 	outputPath := os.Args[3]
-	fileName := os.Args[4]
+	filePath := os.Args[4]
 	pieceIndex := os.Args[5]
 
-	// Read the torrent file to get the tracker URL
-	//nolint:gosec // CLI tool, file path is user-provided argument
-	fileBytes, err := os.ReadFile(fileName)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	torrentInfo, err := metainfo.Parse(fileBytes)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	metaInfo, err := loadTorrent(filePath)
+	die(err)
 
-	// Perform the tracker GET request to get a list of peers
-	peers, err := tracker.GetPeers(torrentInfo)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	conn, err := connectPeer(metaInfo)
+	die(err)
 
-	// Establish a TCP connection with a peer, and perform a handshake
-	conn, _, err := peer.Dial(peers[0], torrentInfo)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
 	defer func() {
 		_ = conn.Close()
 	}()
 
-	err = peer.Setup(conn)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
 	// Download the specified piece from the peer
 	pieceIndexInt, err := strconv.Atoi(pieceIndex)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	piece, err := peer.DownloadPiece(conn, torrentInfo, pieceIndexInt)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	die(err)
+
+	piece, err := peer.DownloadPiece(conn, metaInfo, pieceIndexInt)
+	die(err)
 
 	// Save the downloaded piece to the output path
 	//nolint:gosec // This is a command-line tool. We trust the user to provide a valid file path.
 	err = os.WriteFile(outputPath, piece, 0o644)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	die(err)
 }
 
 func cmdDownload() {
 	if os.Args[2] != "-o" {
-		fmt.Println("Usage: download_piece -o <output_path>")
+		fmt.Fprintln(os.Stderr, "Usage: download_piece -o <output_path>")
 		os.Exit(1)
 	}
 	outputPath := os.Args[3]
 	fileName := os.Args[4]
 
-	// Read the torrent file to get the tracker URL
-	//nolint:gosec // CLI tool, file path is user-provided argument
-	fileBytes, err := os.ReadFile(fileName)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	torrentInfo, err := metainfo.Parse(fileBytes)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	metaInfo, err := loadTorrent(fileName)
+	die(err)
 
-	// Perform the tracker GET request to get a list of peers
-	peers, err := tracker.GetPeers(torrentInfo)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// Establish a TCP connection with a peer, and perform a handshake
-	conn, _, err := peer.Dial(peers[0], torrentInfo)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	conn, err := connectPeer(metaInfo)
+	die(err)
 	defer func() {
 		_ = conn.Close()
 	}()
 
-	err = peer.Setup(conn)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
 	var pieces []byte
-	for pieceIndexInt := 0; pieceIndexInt*torrentInfo.PieceLength < torrentInfo.Length; pieceIndexInt++ {
-		piece, err := peer.DownloadPiece(conn, torrentInfo, pieceIndexInt)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+	for pieceIndexInt := 0; pieceIndexInt*metaInfo.PieceLength < metaInfo.Length; pieceIndexInt++ {
+		piece, err := peer.DownloadPiece(conn, metaInfo, pieceIndexInt)
+		die(err)
 		pieces = append(pieces, piece...)
 	}
 
 	// Save the downloaded piece to the output path
 	//nolint:gosec // This is a command-line tool. We trust the user to provide a valid file path.
 	err = os.WriteFile(outputPath, pieces, 0o644)
+	die(err)
+}
+
+func loadTorrent(path string) (*metainfo.MetaInfo, error) {
+	//nolint:gosec // This is a command-line tool. We trust the user to provide a valid file path.
+	fileBytes, err := os.ReadFile(path)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return nil, err
+	}
+
+	torrentInfo, err := metainfo.Parse(fileBytes)
+	if err != nil {
+		return nil, err
+	}
+	return torrentInfo, nil
+}
+
+func connectPeer(metaInfo *metainfo.MetaInfo) (net.Conn, error) {
+	peers, err := tracker.GetPeers(metaInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	conn, _, err := peer.Dial(peers[0], metaInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	err = peer.Setup(conn)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+func die(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
